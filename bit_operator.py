95754177e182f1b9309f85f9c578a9d4772af107
yusukesakamoto <kataru23@gmail.com>
2022-05-05 11:23:37 +0900

    I'm still not sure about bit operations

--- bit_operator.py
+++ bit_operator.py
@@ -4,7 +4,7 @@ b = 0b10001100
 print("AND",bin(a&b))
 print("OR",bin(a|b))
 print("XOR",bin(a^b)) #^=サーカムフレックスあるいはキャレット
-print("~0",~0)  #~=チルダ
+print("~0",~0)  #~=チルダ　補数に使う
 print("~1",~1)
 print("~-1",~-1)
 print("~-2",~-2)
