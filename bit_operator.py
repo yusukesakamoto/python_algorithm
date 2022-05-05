7861c4f8eec954c3a2d9c7657b51db61aba9f2df
yusukesakamoto <kataru23@gmail.com>
2022-05-05 11:22:35 +0900

    I'm still not sure about bit operations

---
+++ bit_operator.py
@@ -0,0 +1,12 @@
+a = 0b10101001
+b = 0b10001100
+
+print("AND",bin(a&b))
+print("OR",bin(a|b))
+print("XOR",bin(a^b)) #^=サーカムフレックスあるいはキャレット
+print("~0",~0)  #~=チルダ
+print("~1",~1)
+print("~-1",~-1)
+print("~-2",~-2)
+print("0b10101001<<1",bin(a<<1))
+print("0b10001100>>2",bin(b>>2))
