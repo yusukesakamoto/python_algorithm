870c3d1b8acd879155e7ba5d4e3391f1a1e7ab18
yusukesakamoto <kataru23@gmail.com>
2022-05-06 15:46:00 +0900

    I studied bubble sort

---
+++ py_textbook/Lesson5/bubble_sort_2.py
@@ -0,0 +1,10 @@
+data = [9,4,7,2,3,8,6,1,0,5]
+n = len(data)
+print(data,"元のデータ")
+
+for i in range(0,n-1):
+    for j in range(0,n-1):
+        if data[j] > data[j+1]:
+            data[j],data[j+1] = data[j+1],data[j]
+            print(data,"並び替えの過程")
+print(data,"ソート後のデータ")
